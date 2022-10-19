5c50ac9897d71103ebcb99ad04b71997e8c00345
Vatler Lett <business.xfree2000@gmail.com>
2022-10-19 12:06:19 -0700

    prizm-backed

--- routes/api/report.js
+++ routes/api/report.js
@@ -5,74 +5,78 @@ var Report = require('../../models/Report.js');
 
 /* GET ALL Report */
 router.get('/', function(req, res, next) {
-  Report.find(function (err, products) {
-    if (err) return next(err);
-    res.json(products);
-  });
+    Report.find(function(err, products) {
+        if (err) return next(err);
+        res.json(products);
+    });
 });
 
 /* GET SINGLE Report BY ID */
 router.get('/name/:name', function(req, res, next) {
-  Report.find({
-    UserName: req.params.name
-  }, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+    Report.find({
+        UserName: req.params.name
+    }, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
 });
 
 router.get('/date/:date', function(req, res, next) {
-  Report.find({
-    Reg_Date_num: req.params.date
-  }, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+    Report.find({
+        Reg_Date_num: req.params.date
+    }, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
 });
 
 router.get('/:name/:date', function(req, res, next) {
-  Report.find({
-    UserName: req.params.name,
-    Reg_Date_num: req.params.date
-  }, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+    Report.find({
+        UserName: req.params.name,
+        Reg_Date_num: req.params.date
+    }, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
 });
 
 router.get('/:name/:date1/:date2', function(req, res, next) {
-  Report.find({
-    UserName: req.params.name,
-    Reg_Date_num: {$gte:req.params.date1, $lte:req.params.date2}
-  }, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+    Report.find({
+        UserName: req.params.name,
+        Reg_Date_num: { $gte: req.params.date1, $lte: req.params.date2 }
+    }, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
 });
 
 /* SAVE Report */
-router.post('/', function(req, res, next) {   
-  Report.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+router.post('/', function(req, res, next) {
+    Report.create(req.body, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
 });
 
 /* UPDATE Report */
-router.put('/:id', function(req, res, next) {  
-  Report.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+router.put('/:id', function(req, res, next) {
+    Report.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
 });
 
 /* DELETE Report */
 router.delete('/:id', function(req, res, next) {
-  
-  Report.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+
+    Report.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+router.post('/send', function(req, res, next) {
+    console.log(req.body);
 });
 
 module.exports = router;
